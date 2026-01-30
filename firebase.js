a38da9f2e8d6ad978c8d634e5147e3ae4e43d710
devarakonda sri sasank <sasankdevarakonda62@gmail.com>
2026-01-30 19:33:00 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyComvWDZgMUF2_7DaWbppBbPc6AbBNPf3Q",
+  authDomain: "journal-d98dd.firebaseapp.com",
+  projectId: "journal-d98dd",
+  appId: "1:784836352131:web:1553bf1a549dfee50a23b6",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
