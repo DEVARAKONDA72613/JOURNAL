f800c01d87f261f697732688dd23454511a8ef3f
devarakonda sri sasank <sasankdevarakonda62@gmail.com>
2026-01-30 21:01:05 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,13 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
-import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+// firebase.js
+// This file must be in the ROOT (same level as index.html)
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import {
+  getAuth,
+  GoogleAuthProvider
+} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+
+// 🔐 Your Firebase project config
 const firebaseConfig = {
   apiKey: "AIzaSyComvWDZgMUF2_7DaWbppBbPc6AbBNPf3Q",
   authDomain: "journal-d98dd.firebaseapp.com",
@@ -8,7 +15,9 @@ const firebaseConfig = {
   appId: "1:784836352131:web:1553bf1a549dfee50a23b6"
 };
 
+// 🚀 Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
+// 🔑 Auth + Provider
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
